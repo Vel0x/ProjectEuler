c04f09b81f71502fcc1a5877928cc05287b7d77c
Vel0x <DaleMyers19@gmail.com>
2015-03-07 15:30:12 -0800

    Added level 4

---
+++ Level 4/4.py
@@ -0,0 +1,17 @@
+from math import *
+import operator
+
+def is_palindrome(x):
+	sx = str(x)
+	return sx == sx[::-1]
+
+def level4():
+	palindromes = []
+	for x in range(100,999):
+		for y in range(x,999):
+			if is_palindrome(x*y):
+				palindromes.append(x*y)
+	return max(palindromes)
+
+if __name__ == "__main__":
+	print level4()
