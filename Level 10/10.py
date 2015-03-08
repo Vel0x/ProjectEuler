03bd00557683b814a8ff511470f69bd88cd3ecef
Vel0x <DaleMyers19@gmail.com>
2015-03-07 19:24:00 -0800

    Faster version of 10 using sieve

--- Level 10/10.py
+++ Level 10/10.py
@@ -1,17 +1,23 @@
+import math
+
+def primes(maxValue):
+	A = [True]*(maxValue+1)
+	A[0] = False
+	A[1] = False
+	for i in range(2,int(math.sqrt(maxValue))):
+		if A[i]:
+			for j in range(0, maxValue):
+				index = i**2 + j*i
+				if index > maxValue:
+					break
+				A[index] = False
+ 
+	for i in range(0,len(A)):
+		if A[i]:
+			yield i
+
 def level10():
-	primes = [2]
-	i = 3
-	while True:
-		isPrime = True
-		for p in primes:
-			if i%p == 0:
-				isPrime = False
-				break
-		if isPrime:
-			primes.append(i)
-		i += 2
-		if i > 2000000:
-			return sum(primes)
-					
+	return sum(primes(2000000))
+	
 if __name__ == "__main__":
 	print level10()
